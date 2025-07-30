e7774533f9d207c22ac36f8f08e201384ceebf69
rubina02akter <rubinaakter0108@gmail.com>
2025-07-30 21:02:34 +0600

    stu info

---
+++ dist/student.js
@@ -0,0 +1,7 @@
+"use strict";
+let stu = "Rubina";
+let age = 22;
+let admitted = true;
+let subject = 'Economics';
+let reg = 12345;
+console.log(stu, age, admitted, reg);
---
+++ src/student.ts
@@ -0,0 +1,8 @@
+let stu:string = "Rubina";
+let age:number = 22;
+let admitted:boolean = true;
+let subject:string = 'Economics';
+let reg:number = 12345;
+
+
+console.log(stu,age,admitted,reg);
